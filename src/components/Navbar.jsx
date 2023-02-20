c7c3ae916f40cc0c71b60067ad088625621f804a
takuya <hukujuth@gmail.com>
2023-02-20 12:14:50 +0900

    Create the navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarItem from "./NavbarItem";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
+      {/* paramはurlの最後に記載されるもの */}
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
+}
